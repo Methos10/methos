b4d26c1f6f6123d73abcbb0b580272fea2ed2e41
Methos10 <methos10@gmail.com>
2014-12-22 15:29:06 +0100

    Add price slider feature

--- all-cars.tpl.php
+++ all-cars.tpl.php
@@ -1,15 +1,26 @@
 <div ng-app="cars" class="all-cars" ng-controller="carsController">
-	<div class="search">
-		<div class="label"><label>Search:</label></div>
-		<input class="long-search" type="text" ng-model="search.$">
+	<div class="pageinfo">
+		<a href="" ng-click="toggleCustom()" ><div ng-hide="custom">Hide</div><div ng-show="custom">Show</div></a>
+	</div>
+	<div class="search-bar" ng-hide="custom">
+		<div class="search">
+			<div class="label"><label>Search:</label></div>
+			<input class="long-search" type="text" ng-model="search.$">
+		</div>
+		<div class="price-slider">
+				<div class="label"><label>Price:</label></div>			
+				<div class="left">{{lower_price_bound | currency:"€":0}}</div>
+				<div class="right">{{upper_price_bound | currency:"€":0}}</div>
+		        <slider floor="60" ceiling="60000" ng-model-low="lower_price_bound" ng-model-high="upper_price_bound"></slider>
+		</div>
 	</div>
 
-	<div class ="cars" ng-repeat="car in cars | filter:search">
+	<div class ="cars" ng-repeat="car in (filtered = (cars | filter:search | filter:priceFilter ))">
 		<h2><a href="/angular/?q=node/{{ car.node.nid }}">{{ car.node.title }}</a></h2>
 		<div class="cover-image">
 			<a href="/angular/?q=node/{{ car.node.nid }}" title="{{car.node.title}}"><img ng-src="{{car.node.image.src}}"></a>
 		</div>
 		<div class="price"><label>Price: </label> {{ car.node.price | currency:"€ ":0}} </div>		
-		<div class="desc"> {{ car.node.description }} </div>
+		<!-- <div class="desc"> {{ car.node.description }} </div> -->
 	</div>
 </div>
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var cars = angular.module('cars', []);
+var cars = angular.module('cars', ['uiSlider']);
 
 cars.controller('carsController', function($scope, $http) {
 	$http.get('?q=json/cars').success(function(result) {
@@ -6,4 +6,33 @@ cars.controller('carsController', function($scope, $http) {
 			return result.nodes;
 		})();
 	});
+
+    $scope.lower_price_bound = 60;
+    $scope.upper_price_bound = 60000;
+
+    $scope.priceFilter = function (car) {
+      var price = parseFloat(car.node.price);
+      var min = parseFloat($scope.lower_price_bound);
+      var max = parseFloat($scope.upper_price_bound);
+      
+      if (!price) {
+        return false;
+      }
+    
+      if(min && price < min) {
+        return false;
+      }
+      
+      if(max && price > max) {
+        return false;
+      }
+    
+      return true;
+    };
+
+    $scope.custom = false;
+    $scope.toggleCustom = function() {
+        $scope.custom = $scope.custom === false ? true: false;
+    };
+    
 });
